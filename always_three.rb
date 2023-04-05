e14401bdf4aceeb0b518ecde1820914b7adce823
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-05 21:28:54 +0900

    First upload

---
+++ always_three.rb
@@ -0,0 +1,9 @@
+puts ("Give me a number")
+num= gets
+number1= num.to_i
+number1 += 5
+number1 *= 2
+number1 -= 4
+number1 /= 2
+number1 -= num.to_i
+puts ("Always #{number1}")
---
+++ first_script.rb
@@ -0,0 +1,2 @@
+puts "Hellom Word"
+exit
