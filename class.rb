c474b111e0a18e18df9a073e38ddb2290af862d6
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-12 12:14:47 +0900

    Refactory class

--- class.rb
+++ class.rb
@@ -1,20 +1,7 @@
-class Ferret
- 
-	def set_name=(ferret_name)
-		@name = ferret_name
-    end
- 
-	def get_name
-		return @name
-	end
- 
-	def set_owner=(owner_name)
-		@owner_name = owner_name
-	end
- 
-	def get_owner
-		return @owner_name
-	end
+class Pet
+	attr_accessor :name, :owner
+end
+class Ferret < Pet
  
 	def squeal
 		return "squeeeeee"
@@ -23,42 +10,17 @@ class Ferret
 end 
 
  
-class Chincilla
-	def set_name=(chinchilla_name)
-		@name = chinchilla_name
-	end
-    def get_name
-		return @name
-	end
-	def set_owner=(owner_name)
-		@owner_name = owner_name
-	end
-	def get_owner
-		return @owner_name
-	end
+class Chincilla < Pet
+
 	def squeek
 		return "eeeep"
 	end
  
 end
  
-class Parrot
+class Parrot < Pet
  
-	def set_name=(parrot_name)
-		@name = parrot_name
-	end
- 
-	def get_name
-		return @name
-	end
- 
-	def set_owner=(owner_name)
-		@owner_name = owner_name
-	end
- 
-	def get_owner
-		return @owner_name
-	end
+
 	def tweet
         return "eeeep"
     end
@@ -66,16 +28,16 @@ class Parrot
 end
  
 my_ferret = Ferret.new
-my_ferret.set_name= "Fredo"
-ferretname = my_ferret.get_name
+my_ferret.name= "Fredo"
+ferretname = my_ferret.name
  
 my_parrot = Parrot.new
-my_parrot.set_name= "Budgie"
-parrotname = my_parrot.get_name
+my_parrot.name= "Budgie"
+parrotname = my_parrot.name
  
 my_chincilla = Chincilla.new
-my_chincilla.set_name= "Dali"
-chincillaname = my_chincilla.get_name
+my_chincilla.name= "Dali"
+chincillaname = my_chincilla.name
  
 puts "#{ferretname} says #{my_ferret.squeal}, 
 #{parrotname} says #{my_parrot.tweet}, 
